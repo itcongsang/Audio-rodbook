f6d8b0e4cda60d145fe5a451044a6d489c72ef51
ngocdaiza@gmail.com <ngocdaiza@gmail.com>
2019-11-13 15:55:31 +0700

    ok

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -3,20 +3,51 @@ import React from 'react';
 import Link from "next/link";
 
 import Layout from 'components/global/layout';
-import { getAllBooks, getAllBooks2 } from 'services/book.service';
+import { getAllBooks, getAllBooks2, getBooksRecommended } from 'services/book.service';
 import './styles.scss';
 //
 export const Home = () => {
-    const res = useObservable(() => getAllBooks2(),[]);
+    const resGoiY = useObservable(() => getAllBooks2(),[]);
+    const resDeXuat = useObservable(() => getBooksRecommended(),[]);
     return (
     <Layout title="Dashbord" description="This is the home page">
-      <div className="container bt-line">
-        <div className="row">
+        <div className="container bt-line">
+          <div className="row">
             <h2 className="title-oneach">Đề xuất</h2>
+          </div>
+          <div className="row">
+            {console.log(resDeXuat.length)}
+            {resDeXuat.map((t, i) => {
+              return (
+                <div key={i} className="col-md-2 rs-pad mar-bot-25">
+                  <Link href="#">
+                    <a title="">
+                      <div className="img-ct">
+                        <img src={t.coverUrl} className="img-responsive" />
+                      </div>
+                      <div className="main-ct">
+                        <h3><a>{t.name}</a></h3>
+                        <span>{t.channelName}</span>
+                        <div>
+                          <span className="seen-count">{t.rating} lượt xem</span>
+                          <span>1 tuần trước</span>
+                        </div>
+                      </div>
+                    </a>
+                  </Link>
+
+                </div>
+              );
+            })}
+
+          </div>
+        </div>
+        <div className="container bt-line">
+        <div className="row">
+            <h2 className="title-oneach">Gợi ý</h2>
         </div>
         <div className="row">
-          {console.log(res)}
-            {res.map((t, i) => {
+            {resGoiY.map((t, i) => {
             return (
               <div key={i} className="col-md-2 rs-pad mar-bot-25">
                 <Link href="#">
--- services/book.service.ts
+++ services/book.service.ts
@@ -1,5 +1,5 @@
 import { collection, doc, collectionChanges, fromCollectionRef, fromDocRef } from 'rxfire/firestore';
-import { map, switchMap, mergeMap, mergeAll, combineAll } from 'rxjs/operators';
+import { map, switchMap, mergeMap, mergeAll, combineAll, filter } from 'rxjs/operators';
 
 import { db } from './firebase.service';
 import { Book } from 'models/book';
@@ -49,7 +49,7 @@ function getAllBooks2() {
   return collection(db.collection('books').limit(12)).pipe(
     switchMap(books => {
       const channelIds = uniq(books.map(v => v.data()['channelId']));
-      console.log('id ', channelIds)
+      //console.log('id ', channelIds)
       return combineLatest(
         of(books),
         combineLatest(
@@ -73,5 +73,41 @@ function getAllBooks2() {
     })
   );
 }
+function getBooksRecommended() {
+  return collection(db.collection('channel_recommendeds').limit(12)).pipe(
+    switchMap(channels => {
+      const channelIds = uniq(channels.map(v => v.data()['channelId']));
+      console.log('cahnnelIds ', channelIds)
+      return combineLatest(
+        combineLatest(
+          channelIds.map(channelId =>
+            collection(db.collection('books').where('channelId', '==', channelId)).pipe(map(v => v[0]))
+          )
+        ),
+        combineLatest(
+          channelIds.map(channelId =>
+            collection(db.collection('channels').where('channelId', '==', channelId)).pipe(map(v => v[0]))
+          )
+        )
+      );
+    }),
+    map(([books, channels]) => {
+      return channels.map(channel => {
+        const channelDoc = channel.data();
+        const book = books.find(o => (typeof o === 'undefined' ? '' : o.data().channelId) === channelDoc.channelId);
+        console.log(channelDoc)
+        if(typeof book !== 'undefined'){
+          console.log(book.data());
+          return new BookAndChannel({
+            ...book.data(),
+            channelName: typeof channel === 'object' ? channel.data().name : '',
+          });
+        }
+      });
+    }),
+  ).pipe(map(v => {
+    return v.filter(vv => typeof vv !== 'undefined');
+  }));
+}
 
-export { getAllBooks, getAllBooks2 };
+export { getAllBooks, getAllBooks2, getBooksRecommended };

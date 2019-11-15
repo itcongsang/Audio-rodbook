5346fd67421441e576454088c8ec567b4e8b80b9
itcongsang <stitcongsang@gmail.com>
2019-11-15 12:02:27 +0700

    Login
    Thiet ke lai login

--- components/global/layout/menuTop.tsx
+++ components/global/layout/menuTop.tsx
@@ -1,12 +1,14 @@
 // import { useEffect, useState } from 'react';
 import * as React from 'react';
-import { FaBars, FaSearch, FaCloudUploadAlt, FaBell }  from 'react-icons/fa';
+import { FaBars, FaSearch, FaCloudUploadAlt, FaBell, FaUserCircle }  from 'react-icons/fa';
 import Link from 'next/link';
 interface Props {
   clickShow: any ;
+  clickShowModal: any ;
 }
+
 //Nhớ đăng ký Test lên file index component
-const HeaderTop: React.SFC<Props> =({clickShow}) => {
+const HeaderTop: React.SFC<Props> =({clickShow, clickShowModal}) => {
   return (
     <div className="bg-w h-limit">
       <div className="container">
@@ -39,11 +41,15 @@ const HeaderTop: React.SFC<Props> =({clickShow}) => {
                 <span className="bell-notiface">1</span>
               </div>
               <div className="in-line ds-mn">
-              <Link href="login">
+                <div className="login-button" onClick={clickShowModal}>
+                  <FaUserCircle className="" />
+                  <span>ĐĂNG NHẬP</span>
+                </div>
+              {/* <Link href="login">
                 <a className="a-pon">
                   <img src="https://yt3.ggpht.com/a-/AAuE7mAkkx-gP5K9_5EvJvnD-jfhWRrIb6pKTSinMA=s88-c-k-c0x00ffffff-no-rj-mo" className="img-responsive img-pon" />
                 </a>
-              </Link>
+              </Link> */}
               </div>
             </div>
           </div>
